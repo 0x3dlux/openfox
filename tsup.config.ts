160bb32bf8ccdc31af655de7e37b1f7bda0713bd
conrad <c.lelubre@gmail.com>
2026-06-21 17:27:11 +0300

    fix: tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -17,5 +17,5 @@ export default defineConfig({
   dts: true,
   clean: true,
   sourcemap: true,
-  external: ['better-sqlite3', 'vscode-jsonrpc', 'vscode-languageserver-protocol', 'ws', 'node-pty'],
+  external: ['better-sqlite3', 'vscode-jsonrpc', 'vscode-languageserver-protocol', 'ws', 'node-pty', 'undici'],
 })
